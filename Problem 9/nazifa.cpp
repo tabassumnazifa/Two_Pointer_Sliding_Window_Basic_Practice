e63bcbbf24510a0bcb0a69e1fe82bc468e02e437
Nazifa Tabassum <132453499+tabassumnazifa@users.noreply.github.com>
2025-08-26 09:39:38 +0600

    Add files via upload

---
+++ Problem 9/nazifa.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int removeDuplicates(vector<int>& nums)
+    {
+        if (nums.empty()) return 0;
+
+        int k = 0;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] != nums[k])
+            {
+                nums[k+1] = nums[i];
+                k++;
+            }
+        }
+
+        return k + 1;
+    }
+};
